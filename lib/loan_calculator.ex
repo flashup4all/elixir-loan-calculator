aaf842d0cf0c8e2c039632b738de5c2ed8494c5f
Bardeson Lucky <mac@macs-MacBook-Pro.local>
2020-02-07 11:02:01 +0100

    fix: loan_calculator_test

--- lib/loan_calculator.ex
+++ lib/loan_calculator.ex
@@ -8,16 +8,16 @@ defmodule LoanCalculator do
 
   ## Examples
 
-      iex> LoanCalculator.calculator()
-      :world
+      iex> LoanCalculator.calculator(10, 5000, 30)
+      5519.4318039119225
 
   """
-  def calculator(interest, amount, days) do
+  def calculator(interest, amount, no_of_days) do
     interest_per_day = ((interest/100)/30) |> Float.floor(4) 
     total_interest = interest_per_day * amount
     total_balance = total_interest + amount
-    case days > 0 do
-      true  -> calculator(interest, total_balance, days-1)
+    case no_of_days > 0 do
+      true  -> calculator(interest, total_balance, no_of_days-1)
       false ->  amount
     end
   end
--- test/loan_calculator_test.exs
+++ test/loan_calculator_test.exs
@@ -2,7 +2,7 @@ defmodule LoanCalculatorTest do
   use ExUnit.Case
   doctest LoanCalculator
 
-  test "greets the world" do
-    assert LoanCalculator.hello() == :world
+  test "calculate loan balance" do
+    assert LoanCalculator.calculator(10, 5000, 30) == 5519.4318039119225
   end
 end
